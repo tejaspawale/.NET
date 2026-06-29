1293fb14f5b9c8bb5a3bfbd09d6d1c26e0482684
TEJAS PAWALE <pawaletejas98@gmail.com>
2026-06-29 14:39:00 +0530

    2d 2 array addition logic with hardcoded and with loop

--- Dsa/ArraysProject/Program.cs
+++ Dsa/ArraysProject/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] numbers =
+{
+    {1,2,3},
+    {4,5,6}
+};
+
+
+int[,] numbers2 =
+{
+    {7,8,9},
+    {10,11,12}
+};
+
+Console.WriteLine(numbers[0,0] + numbers2[0,0]);
+Console.WriteLine(numbers[0,1] + numbers2[0,1]);
+Console.WriteLine(numbers[0,2] + numbers2[0,2]);
+
+Console.WriteLine(numbers[1,0] + numbers2[1,0]);
+Console.WriteLine(numbers[1,1] + numbers2[1,1]);
+Console.WriteLine(numbers[1,2] + numbers2[1,2]);
+
+for(int i = 0; i<=1 ; i++)
+{
+    for(int j = 0; j<=2; j++)
+    {
+        Console.Write((numbers[i,j] + numbers2[i,j] +" "));
+    }
+    Console.WriteLine();
+
+}
