5f40af8e49b8c8a40c14a38f259c0fdbc94b5339
TEJAS PAWALE <pawaletejas98@gmail.com>
2026-07-13 13:10:13 +0530

    ReverseEachWordSentence

---
+++ Problems/string/ReverseEachWordSentence/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace String;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string sentence = "Build Application";
+        string[] words = sentence.Split(" ");
+      
+
+        for(int i=0; i<words.Length; i++)
+        {     string reverseString = " ";
+            for(int j = words[i].Length - 1; j>=0; j--)
+            {
+                reverseString += words[i][j];
+            }
+            words[i]=reverseString;
+        }
+        Console.WriteLine(string.Join(" ",words));
+
+    }
+}
