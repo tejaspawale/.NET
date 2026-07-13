d8a111e4a36fd14a2665116104ef4a022982f904
TEJAS PAWALE <pawaletejas98@gmail.com>
2026-07-13 11:56:55 +0530

    String Probelmes added length of string

---
+++ Problems/string/LengthOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StringProblems;
+
+public class Program
+{
+
+
+
+    public static void Main(string[] args)
+    {
+        string name = "Tejas";
+        int count = 0;
+        foreach (var i in name)
+        {
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+
+}
