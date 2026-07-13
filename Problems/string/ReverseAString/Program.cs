c5b16f7d0a82bfbccc374eb4a881303988ad9b79
TEJAS PAWALE <pawaletejas98@gmail.com>
2026-07-13 12:17:25 +0530

    Reverise Astring

--- Problems/string/ReverseAString/Program.cs
+++ Problems/string/ReverseAString/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace strings;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string str = "Anish adak";
+        string revStr = "";
+
+        for(int i=str.Length-1; i>=0; i--)
+        {
+            revStr += str[i];
+            
+        }
+        Console.WriteLine(revStr);
+
+    }
+}
